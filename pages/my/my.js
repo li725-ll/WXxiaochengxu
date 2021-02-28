a5550b1ac83c79d7fef71661be16c7b44a934419
li725-ll <65399541+li725-ll@users.noreply.github.com>
2021-02-28 16:55:12 +0800

    Add files via upload

---
+++ pages/my/my.js
@@ -0,0 +1,85 @@
+// pages/my/my.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),//判断是否可用
+    imagePath:"https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.51yuansu.com%2Fpic3%2Fcover%2F01%2F69%2F80%2F595f67c2aff1e_610.jpg&refer=http%3A%2F%2Fpic.51yuansu.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1617086093&t=42e3471115b85766c4a124c232887023",
+    userName:"用户名",
+    flag:true,
+    menu:["账户","关注","设置","关于"]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  /**
+   * 获取用户信息
+   */
+  bindGetUserInfo:function(){
+    var that = this
+    wx.getUserInfo({
+      success:function(res){
+        that.setData({imagePath:res.userInfo.avatarUrl})
+        that.setData({userName:res.userInfo.nickName})
+        that.setData({flag:false})
+      }
+    })
+    
+  }
+})
