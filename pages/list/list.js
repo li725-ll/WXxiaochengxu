05fb02f1153dc12ed502bf88a79153f6f969409c
li725-ll <65399541+li725-ll@users.noreply.github.com>
2021-02-28 16:52:06 +0800

    Add files via upload

---
+++ pages/list/list.js
@@ -0,0 +1,88 @@
+// pages/list/list.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    music: [{
+      musicname: "需要人陪",
+      author: "王力宏",
+      musicsrc: "http://music.163.com/song/media/outer/url?id=25643291.mp3",
+      musicimage: "https://y.gtimg.cn/music/photo_new/T002R300x300M000002iWU6B2ZvA8V_1.jpg?max_age=2592000"
+    },
+    {
+      musicname: "成都",
+      author: "赵雷",
+      musicsrc: "http://music.163.com/song/media/outer/url?id=436514312.mp3",
+      musicimage: "https://y.gtimg.cn/music/photo_new/T002R300x300M000000jE4g74VS43p_1.jpg?max_age=2592000"
+    },
+    {
+      musicname: "monsters",
+      author: "Katie Sky",
+      musicsrc: "http://music.163.com/song/media/outer/url?id=28461933.mp3",
+      musicimage: "https://y.gtimg.cn/music/photo_new/T002R300x300M000001Z5XGk3onxuW_1.jpg?max_age=2592000"
+    },
+    {
+      musicname: "像鱼",
+      author: "王贰浪",
+      musicsrc: "http://music.163.com/song/media/outer/url?id=1331819951.mp3",
+      musicimage: "https://y.gtimg.cn/music/photo_new/T002R300x300M000001Zrk6t0urxvG_1.jpg?max_age=2592000"
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
